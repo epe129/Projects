042c2d09281a043695c68e84d46fe2b17b3bc242
epe129 <eemelik985@gmail.com>
2025-10-01 19:54:25 +0300

    commit

--- verkkokauppa-django/venv2/myapp/theapp/views.py
+++ verkkokauppa-django/venv2/myapp/theapp/views.py
@@ -11,8 +11,43 @@ def products(request):
 
     with open('products.json', 'r') as file:
         data = json.load(file)    
+    
+    if 'orders' in request.POST:
+        orders = request.POST.get("orders") 
+        data1 = orders.split()
 
-    return render(request, 'products.html',  {"json_data": data})
+        write_oders = {
+            "orderid" : f"{data1[0]}", 
+            "malli" : f"{data1[1]}", 
+            "hinta" : f"{data1[2]}" 
+        }
+        # appends orders in json file
+        with open("orders.json", "r+") as f:
+            file_data = json.load(f)
+            for key, value in file_data.items():
+                for a in value:
+                    if a["orderid"] == orders:
+                        orders = ""
+            
+            if orders == "":
+                pass
+            else:
+                file_data["orders"].append(write_oders)
+                                
+                f.seek(0)
+                                
+                json.dump(file_data, f, indent=4)     
+    
+    
+    with open('orders.json', 'r') as file:
+        ordersdata = json.load(file)    
+    
+    # for keyy, valuee in ordersdata.items():
+    #     for xx in valuee:
+    #         print(xx)
+
+
+    return render(request, 'products.html',  {"json_data": data, "o_data": ordersdata})
 
 def cart(request):
 
