58325dfc42c791000b181877f1ff313a31d82bee
epe129 <eemelik985@gmail.com>
2025-11-01 10:19:38 +0200

    commit

--- reseptit-sovellus/venv/static/js/index.js
+++ reseptit-sovellus/venv/static/js/index.js
@@ -9,3 +9,27 @@ function myadd() {
         t = 0                
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rajaanappi = document.querySelectorAll("#ul li");
+    const reseptidiv = document.querySelectorAll(".recipe-card");
+
+    rajaanappi.forEach(button => {
+        button.addEventListener("click", () => {
+            const kategoria = button.textContent.trim().toLowerCase();
+
+            reseptidiv.forEach(card => {
+                const ruokaKatekoria = card.dataset.category.toLowerCase();
+                
+
+                if (kategoria === "all" || ruokaKatekoria === kategoria) {
+                    card.style.display = "block"
+                    ;
+                } else {
+                    card.style.display = "none";
+                }
+
+            });
+        });
+    });
+});
