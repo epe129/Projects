ff1dfcc6904eb99fb76c2d665f977d83f7b9c8e5
epe129 <eemelik985@gmail.com>
2025-12-06 13:15:59 +0200

    commit

--- math-game/venv/static/script.js
+++ math-game/venv/static/script.js
@@ -1,41 +1,125 @@
 let laskuNäytä = document.getElementById("lasku");
 let merkit = ["+","-","/","*"]
-let vastaus = 0;
+let vastaus = [];
+
 let ekaNumero = Math.floor(Math.random() * 10) + 1;
 let tokaNumero = Math.floor(Math.random() * 10) + 1;
+let kolmasNumero = Math.floor(Math.random() * 10) + 1;
+
 let randomMerkki = Math.floor(Math.random() * merkit.length);
+let randomMerkki2 = Math.floor(Math.random() * merkit.length);
+
 let merkki = merkit[randomMerkki];
+
+let merkki2 = merkit[randomMerkki2];
+
+laskuNäytä.innerHTML = `Lasku: ${ekaNumero} ${merkki} ${tokaNumero} ${merkki2} ${kolmasNumero}`
+
+vastaus.push(ekaNumero)
     
-laskuNäytä.innerHTML = `Lasku: ${ekaNumero} ${merkki} ${tokaNumero}`
+vastaus.push(merkki)
+    
+vastaus.push(tokaNumero)
+
+vastaus.push(merkki2)
+
+vastaus.push(kolmasNumero)
+
+
+let v = document.getElementById("vastaus");
+let n = document.getElementById("Onko");
+    
+
+let etsiKertomerkki;
+let etsiJakomerkki;
+let merkkiIndex;
+let tulos = 0;
 
 function naytaVastaus() {
+    let inputShow = v.value
 
-    const v = document.getElementById("vastaus");
-    const n = document.getElementById("Onko");
-    
-    const inputShow = v.value
+    for (let i = 0; i < vastaus.length; i++) {    
+        
+        etsiJakomerkki = vastaus.includes("/")        
+        etsiKertomerkki = vastaus.includes("*")
+       
+        if (etsiJakomerkki == true || etsiKertomerkki == true) {
+            switch(vastaus[i]) {
+                case "/":
+                    merkkiIndex = vastaus.indexOf("/")
 
-    switch(merkki) {
-        case "+":
-            vastaus = ekaNumero + tokaNumero;
-            break;
-        case "-":
-            vastaus = ekaNumero - tokaNumero;
-            break;
-        case "/":
-            vastaus = ekaNumero / tokaNumero;
-            break;       
-        case "*":
-            vastaus = ekaNumero * tokaNumero;
-            break;
-        }
+                    tulos = vastaus[merkkiIndex-1] / vastaus[merkkiIndex+1];
+                    
+                    console.log(vastaus[merkkiIndex-1] , vastaus[merkkiIndex+1])
+                    
+                    vastaus.splice(merkkiIndex-1, merkkiIndex+2, tulos);
+                        
+                    console.log(vastaus)
 
-    if (parseFloat(inputShow) == parseFloat(vastaus)) {
-        n.innerHTML = "Oikein: " + vastaus;
+                    i = 0;
+                    tulos = 0
+                    merkkiIndex = 0
+
+                    etsiJakomerkki = vastaus.includes("/")
+
+                    break;       
+                case "*":
+                    merkkiIndex = vastaus.indexOf("*")
+                        
+                    tulos = vastaus[merkkiIndex-1] * vastaus[merkkiIndex+1];
+                        
+                    vastaus.splice(merkkiIndex-1, merkkiIndex+2, tulos);
+                        
+                    console.log(vastaus)
+                        
+                    i = 0;
+                    tulos = 0
+                    merkkiIndex = 0
+                    etsiKertomerkki = vastaus.includes("*")
+
+                    break;
+                }
+            } else {
+                switch(vastaus[i]) {
+                    case "+":
+                        merkkiIndex = vastaus.indexOf("+")
+                            
+                        tulos = vastaus[merkkiIndex-1] + vastaus[merkkiIndex+1];
+                            
+                        vastaus.splice(merkkiIndex-1, merkkiIndex+2, tulos);
+                            
+                        console.log(vastaus)
+                            
+                        i--;
+                        tulos = 0
+                        merkkiIndex = 0
+                            
+                        break;
+                    case "-":
+                        merkkiIndex = vastaus.indexOf("-")
+                            
+                        tulos = vastaus[merkkiIndex-1] - vastaus[merkkiIndex+1];
+                            
+                        vastaus.splice(merkkiIndex-1, merkkiIndex+2, tulos);
+                            
+                        console.log(vastaus)
+                            
+                        i--;
+                        tulos = 0
+                        merkkiIndex = 0
+                            
+                        break;
+                    }
+                }
+    }
+
+    if (parseFloat(inputShow) == parseFloat(vastaus[0])) {
+        n.innerHTML = "Oikein: " + vastaus[0];
     } else {
         console.log(vastaus)
-        n.innerHTML = "Väärin: " + vastaus;
+        n.innerHTML = "Väärin: " + vastaus[0];
     }
+
 }
     
 
