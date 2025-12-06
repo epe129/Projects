4878bb8dc8154e87c82b2d8c9b01120f339ddf42
epe129 <eemelik985@gmail.com>
2025-12-06 15:41:39 +0200

    commit

--- math-game/venv/app.py
+++ math-game/venv/app.py
@@ -10,5 +10,21 @@ def index():
 def peruslaskuja():
     return render_template("/pelit/peruslaskut.html")
 
+@app.route('/peruslaskuja/Pluslaskuja')
+def Pluslaskuja():
+    return render_template("/pelit/Pluslaskuja.html")
+
+@app.route('/peruslaskuja/Miinuslaskuja')
+def Miinuslaskuja():
+    return render_template("/pelit/Miinuslaskuja.html")
+
+@app.route('/peruslaskuja/Kertolaskuja')
+def Kertolaskuja():
+    return render_template("/pelit/Kertolaskuja.html")
+
+@app.route('/peruslaskuja/Jakolaskuja')
+def Jakolaskuja():
+    return render_template("/pelit/Jakolaskuja.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
