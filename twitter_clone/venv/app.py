d463bc8627f548a252c2b2a223471edda34bf5a9
epe129 <eemelik985@gmail.com>
2025-12-31 14:38:15 +0200

    commit

--- twitter_clone/venv/app.py
+++ twitter_clone/venv/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
-import json
 import sqlite3
 
 app = Flask(__name__)
@@ -12,12 +11,15 @@ Session(app)
 
 @app.route('/')
 def index():
-
     return render_template("index.html")
 
 @app.route('/page', methods =["GET", "POST"])
 def page():
-    username = ""
+
+    if not session.get("username"):
+        return redirect("/login")
+    
+    username = session.get("username")
     tweet = ""
     user = ""
     PrivateOrPublic = ""
@@ -27,10 +29,6 @@ def page():
 
     c = conn.cursor()
 
-    if not session.get("username"):
-        return redirect("/login")
-    else:
-        username = session.get("username")
     
     if request.method == "POST":
         tweet = request.form.get("tweet")
@@ -63,51 +61,50 @@ def page():
             continue
         elif c[1] == "Public":
             data.append(c)
-    
-    # print(data)
-    
+        
     data.sort()
 
     p = len(data)
 
-
     return render_template("/pages/Thepage.html", username=username, data=data, p=p)
-
 
 @app.route('/login', methods =["GET", "POST"])
 def login():
+
     conn = sqlite3.connect('/home/lenni/home/koodit/projects/twitter_clone/venv/db/users.db')
 
     c = conn.cursor()
-
-    c.execute("SELECT * FROM users")
-
-    rows = c.fetchall()
-    
+       
     if request.method == "POST":
+        
         username = request.form.get("username")
+        
         session["username"] = username
+        
         password = request.form.get("pwd") 
+        
         NotExist = "User not exist"
+
+        c.execute("SELECT * FROM users")
+
+        rows = c.fetchall()
        
         for rivi in rows:
             if username == rivi[0] and password == rivi[1]:
                 return redirect("page")
             else:
-                return render_template("/pages/login.html", NotExist=NotExist)                
+                continue              
     
         c.close()
+        
+        return render_template("/pages/login.html", NotExist=NotExist)     
 
     return render_template("/pages/login.html")
 
+
 @app.route('/register', methods =["GET", "POST"])
 def register():
-    username = ""
-    password = ""
-    passwordConfirm = ""
-
     SameUsername = "Username is taken"
-    PasswordMatch = "Password not maching"
     empty = "Username or password field is empty"
         
     conn = sqlite3.connect('/home/lenni/home/koodit/projects/twitter_clone/venv/db/users.db')
@@ -115,15 +112,13 @@ def register():
     c = conn.cursor()
 
     if request.method == "POST":
-        username = request.form.get("username")
-        session["username"] = username
-        password = request.form.get("pwd") 
-       
-        passwordConfirm = request.form.get("pwdConfirm")
-          
-        if password != passwordConfirm:
-            return render_template("/pages/register.html", PasswordMatch=PasswordMatch)            
         
+        username = request.form.get("username")
+        
+        session["username"] = username
+        
+        password = request.form.get("pwd")        
+          
         if username == "" or password == "":
             return render_template("/pages/register.html", empty=empty)
                     
@@ -147,10 +142,6 @@ def register():
         conn.commit()
 
         return redirect("page")
-        
-    username = ""
-    password = ""
-    passwordConfirm = ""
     
     return render_template("/pages/register.html")
 
@@ -158,6 +149,9 @@ def register():
 def profile(username):
     data = []
 
+    if not session.get("username"):
+        return redirect("/login")
+    
     conn = sqlite3.connect('/home/lenni/home/koodit/projects/twitter_clone/venv/db/tweets.db')
 
     c = conn.cursor()
@@ -166,7 +160,6 @@ def profile(username):
 
     rows = c.fetchall()
     
-
     for c in rows:
         if c[0] == username:
             data.append(c)
@@ -175,9 +168,12 @@ def profile(username):
 
     p = len(data)
 
-
     return render_template('/pages/profile.html', username=username, data=data, p=p)
-    
+
+@app.route("/logout")
+def logout():    
+    session.clear()
+    return redirect("/")
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run()
